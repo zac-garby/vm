ba96e62578d92e564b69e2e4f306d4c274ef9675
Zac Garby <me@zacgarby.co.uk>
2021-01-15 18:05:22 +0000

    make a load of new instructions

--- src/bytecode.h
+++ src/bytecode.h
@@ -10,15 +10,24 @@ typedef unsigned char byte;
 #define I_SWAP 3
 
 // data instructions
-#define I_CONST 10
-#define I_LOAD_VAR 11
-#define I_STORE_VAR 12
+#define I_LOAD_CONST 10
+#define I_LOAD_LOCAL 11
+#define I_STORE_LOCAL 12
+#define I_LOAD_GLOBAL 13
+#define I_STORE_GLOBAL 14
 #define I_NEW_LIST 13
 
 // operation instructions
-#define I_ADD 20
-#define I_SUB 21
-#define I_MUL 22
-#define I_DIV 23
+#define I_ADD 40
+#define I_SUB 41
+#define I_MUL 42
+#define I_DIV 43
+
+// control instructions
+#define I_JUMP 80
+#define I_LJUMP 81
+#define I_JUMP_IF 82
+#define I_LJUMP_IF 83
+#define I_RETURN 84
 
 #endif

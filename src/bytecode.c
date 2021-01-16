bccda758331ef2d8f74b2c5b6c8141b29976f265
Zac Garby <me@zacgarby.co.uk>
2021-01-16 11:59:51 +0000

    define the amount of arguments for each instruction

--- src/bytecode.c
+++ src/bytecode.c
@@ -1,1 +1,15 @@
 #include "bytecode.h"
+
+byte VM_N_ARGS[256] = {
+    // stack instructions
+    0, 0, 0, 0, NA,NA,NA,NA,NA,NA,
+
+    // data instructions
+    1, 1, 1, 1, 1, 1, NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
+    
+    // operation instructions
+    0, 0, 0, 0, NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,
+    
+    // control instructions
+    1, 2, 1, 2, 1,
+};
--- src/bytecode.h
+++ src/bytecode.h
@@ -1,6 +1,10 @@
 #ifndef H_VM_BYTECODE
 #define H_VM_BYTECODE
 
+// the amount of args that a non-existent instruction has.
+// just a placeholder.
+#define NA 255
+
 typedef unsigned char byte;
 
 // stack instructions
@@ -15,19 +19,21 @@ typedef unsigned char byte;
 #define I_STORE_LOCAL 12
 #define I_LOAD_GLOBAL 13
 #define I_STORE_GLOBAL 14
-#define I_NEW_LIST 13
+#define I_NEW_LIST 15
 
 // operation instructions
-#define I_ADD 40
-#define I_SUB 41
-#define I_MUL 42
-#define I_DIV 43
+#define I_ADD 30
+#define I_SUB 31
+#define I_MUL 32
+#define I_DIV 33
 
 // control instructions
-#define I_JUMP 80
-#define I_LJUMP 81
-#define I_JUMP_IF 82
-#define I_LJUMP_IF 83
-#define I_RETURN 84
+#define I_JUMP 50
+#define I_LJUMP 51
+#define I_JUMP_IF 52
+#define I_LJUMP_IF 53
+#define I_RETURN 54
+
+byte VM_N_ARGS[256];
 
 #endif

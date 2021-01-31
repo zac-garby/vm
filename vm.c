f327bf58c52fc31014308e91629ce8d8b186cbba
Zac Garby <me@zacgarby.co.uk>
2021-01-31 16:17:28 +0000

    example list append program

--- vm.c
+++ vm.c
@@ -13,13 +13,13 @@
 int main() {
     vm_funcobj main, inc;
     
-    vm_obj *the_arg = malloc(sizeof(vm_obj));
-    vm_new_int(the_arg, 10);
+    //vm_obj *the_arg = malloc(sizeof(vm_obj));
+    //vm_new_int(the_arg, 10);
     
     main.name = "main";
     main.arity = 0;
-    main.code = malloc(11);
-    main.code_length = 10;
+    main.code = malloc(12);
+    main.code_length = 12;
     main.consts = malloc(sizeof(vm_obj) * 2);
     main.num_consts = 2;
     main.names = malloc(sizeof(char*) * 1);
@@ -28,19 +28,21 @@ int main() {
 
     main.code[0] = I_LOAD_CONST;
     main.code[1] = 0;
-    main.code[2] = I_LOAD_CONST;
-    main.code[3] = 1;
-    main.code[4] = I_CALL;
-    main.code[5] = I_STORE_LOCAL;
-    main.code[6] = 0;
-    main.code[7] = I_LOAD_LOCAL;
-    main.code[8] = 0;
-    main.code[9] = I_DEBUG;
+    main.code[2] = I_STORE_LOCAL;
+    main.code[3] = 0;
+    main.code[4] = I_LOAD_LOCAL;
+    main.code[5] = 0;
+    main.code[6] = I_LOAD_CONST;
+    main.code[7] = 1;
+    main.code[8] = I_CALL;
+    main.code[9] = I_LOAD_LOCAL;
+    main.code[10] = 0;
+    main.code[11] = I_DEBUG;
 
     inc.name = "inc";
     inc.arity = 1;
-    inc.code = malloc(8);
-    inc.code_length = 8;
+    inc.code = malloc(9);
+    inc.code_length = 9;
     inc.consts = malloc(sizeof(vm_obj) * 1);
     inc.num_consts = 1;
     inc.names = malloc(sizeof(char*) * 1);
@@ -52,12 +54,13 @@ int main() {
     inc.code[2] = 0;
     inc.code[3] = I_LOAD_CONST;
     inc.code[4] = 0;
-    inc.code[5] = I_ADD;
-    inc.code[6] = I_DEBUG;
-    inc.code[7] = I_RETURN;
+    inc.code[5] = I_DEBUG;
+    inc.code[6] = I_LIST_APPEND;
+    inc.code[7] = I_DEBUG;
+    inc.code[8] = I_RETURN;
 
     vm_new_int(&inc.consts[0], 1);
-    vm_new_int(&main.consts[0], 5);
+    vm_new_list(&main.consts[0], 5);
     vm_new_func(&main.consts[1], inc);
 
     vm_thread th = vm_new_thread(0);
